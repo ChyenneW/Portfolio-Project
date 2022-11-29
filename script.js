76f9b20ca7fa5f47d3305e2c324de7a813729ab6
ChyenneW <williamscheyenne771@gmail.com>
2022-11-29 09:22:15 -0600

    Add function to hide navigation

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function show() {
     document.querySelector('.hamburger').classList.toggle('open')
     document.querySelector('.navigation').classList.toggle('active')
 }
+
+const navLinks = document.getElementById('navLinks');
+
+window.addEventListener("scroll", () => {
+    var y = window.scrollY;
+    if (y >= 130) {
+        navLinks.classList.add('disappear');
+        return;
+    }
+    else {
+        navLinks.classList.remove('disappear');
+    }
+});
